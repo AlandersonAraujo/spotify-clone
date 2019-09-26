1e476cda4e2cd4383978ddfa35e3cb6f99695b0a
AlandersonAraujo <alandersoncaraujo@gmail.com>
2019-09-26 11:44:26 -0300

    home

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import Icon from 'react-native-vector-icons/EvilIcons';
-import {Container, ContainerHeader, Title, TabsContainer, TabItem, TabText, Tabs, Image} from './styles';
+import {Container, ContainerHeader, Title, TabsContainer, TabItem, TabText, Tabs, Image, TabItemContainer, Image2, Tabs2, TabText2, TabItemContainer2, Image3} from './styles';
 import logo from '../../assets/hipsters.png';
+import logo2 from '../../assets/nerdcast.png';
+import logo3 from '../../assets/pac.png';
 
 export default class Inicio extends Component {
   render() {
@@ -36,6 +38,121 @@ export default class Inicio extends Component {
             </TabItem>
             <TabText>Hipsters Ponto Tech</TabText>
           </Tabs>
+          <Tabs>
+            <TabItem>
+            <Image source={logo} />
+            </TabItem>
+            <TabText>Hipsters Ponto Tech</TabText>
+          </Tabs>
+          <Tabs>
+            <TabItem>
+            <Image source={logo} />
+            </TabItem>
+            <TabText>Hipsters Ponto Tech</TabText>
+          </Tabs>
+          <Tabs>
+            <TabItem>
+            <Image source={logo} />
+            </TabItem>
+            <TabText>Hipsters Ponto Tech</TabText>
+          </Tabs>
+      </TabsContainer>
+      <Title>Seus podcasts mais escutados</Title>
+      <TabsContainer>
+          <Tabs2>
+            <TabItemContainer>
+            <Image2 source={logo2} />
+            </TabItemContainer>
+            <TabText>Nerdcast</TabText>
+            <TabText2>Jovem Nerd</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer>
+            <Image2 source={logo2} />
+            </TabItemContainer>
+            <TabText>Nerdcast</TabText>
+            <TabText2>Jovem Nerd</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer>
+            <Image2 source={logo2} />
+            </TabItemContainer>
+            <TabText>Nerdcast</TabText>
+            <TabText2>Jovem Nerd</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer>
+            <Image2 source={logo2} />
+            </TabItemContainer>
+            <TabText>Nerdcast</TabText>
+            <TabText2>Jovem Nerd</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer>
+            <Image2 source={logo2} />
+            </TabItemContainer>
+            <TabText>Nerdcast</TabText>
+            <TabText2>Jovem Nerd</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer>
+            <Image2 source={logo2} />
+            </TabItemContainer>
+            <TabText>Nerdcast</TabText>
+            <TabText2>Jovem Nerd</TabText2>
+          </Tabs2>
+      </TabsContainer>
+      <Title>Suas músicas e álbuns favoritos</Title>
+      <TabsContainer>
+          <Tabs2>
+            <TabItemContainer2>
+            <Image3 source={logo3} />
+            </TabItemContainer2>
+            <TabText>All Eyez On Me</TabText>
+            <TabText2>2Pac</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer2>
+            <Image3 source={logo3} />
+            </TabItemContainer2>
+            <TabText>All Eyez On Me</TabText>
+            <TabText2>2Pac</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer2>
+            <Image3 source={logo3} />
+            </TabItemContainer2>
+            <TabText>All Eyez On Me</TabText>
+            <TabText2>2Pac</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer2>
+            <Image3 source={logo3} />
+            </TabItemContainer2>
+            <TabText>All Eyez On Me</TabText>
+            <TabText2>2Pac</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer2>
+            <Image3 source={logo3} />
+            </TabItemContainer2>
+            <TabText>All Eyez On Me</TabText>
+            <TabText2>2Pac</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer2>
+            <Image3 source={logo3} />
+            </TabItemContainer2>
+            <TabText>All Eyez On Me</TabText>
+            <TabText2>2Pac</TabText2>
+          </Tabs2>
+          <Tabs2>
+            <TabItemContainer2>
+            <Image3 source={logo3} />
+            </TabItemContainer2>
+            <TabText>All Eyez On Me</TabText>
+            <TabText2>2Pac</TabText2>
+          </Tabs2>
       </TabsContainer>
       </Container>
     );
--- src/pages/Inicio/styles.js
+++ src/pages/Inicio/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
   flex: 1;
   background: #292929;
 `;
@@ -9,11 +9,11 @@ export const ContainerHeader = styled.View`
   display: flex;
   margin-left: 370px;
   margin-top: 10px;
+  margin-bottom: 20px;
 `;
 
 export const Title = styled.Text`
   margin-left: 18px;
-  margin-top: 20px;
   font-family: Arial;
   color: #FFF;
   font-size: 21px;
@@ -56,5 +56,52 @@ export const Image = styled.Image`
   width: 100px;
   height: 105px;
   border-radius: 10px;
+  position: absolute;
+`;
+
+export const TabItemContainer = styled.View`
+  width: 160px;
+  height: 160px;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 10px;
+  margin-left: 10px;
+  padding: 10px;
+  justify-content: space-between;
+`;
+
+export const Image2 = styled.Image`
+  width: 160px;
+  height: 160px;
+  border-radius: 10px;
+  position: absolute;
+`;
+
+export const Tabs2 = styled.View`
+  display: flex;
+  width: 160px;
+  margin-right: 14px;
+`;
+
+export const TabText2 = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  font-size: 11px;
+  color: #7a7a7a;
+  margin-left: 10px;
+  margin-top: 1px;
+`;
+
+export const TabItemContainer2 = styled.View`
+  width: 160px;
+  height: 160px;
+  background: rgba(255, 255, 255, 0.2);
+  margin-left: 10px;
+  padding: 10px;
+  justify-content: space-between;
+`;
+
+export const Image3 = styled.Image`
+  width: 160px;
+  height: 160px;
   position: absolute;
 `;
